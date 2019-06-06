eaa3c225eaee62b31cf5a5b9b01b83bd229b8f58
prestonlindhardt <46542413+prestonlindhardt@users.noreply.github.com>
2019-06-06 12:48:42 -0600

    Add files via upload

---
+++ js/variables.js
@@ -0,0 +1,4 @@
+var section_content = document.getElementById("section_content");
+console.log(section_content);
+var input_btn_Location_1 = document.getElementById("input_btn_Location_1");
+console.log(input_btn_Location_1);
